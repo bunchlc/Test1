d782bad8ad2c694e238406ccf03e690c510b6c6a
Chris Bowen <50932159+mmxchris@users.noreply.github.com>
2020-02-01 17:37:49 -0500

    added my message

--- src/bunch/Main.java
+++ src/bunch/Main.java
@@ -3,5 +3,6 @@ package bunch;
 public class Main {
     public static void main (String[] args) {
         System.out.println("Hello From Lance");
+        System.out.println("Hello From Chris :D");
     }
 }
